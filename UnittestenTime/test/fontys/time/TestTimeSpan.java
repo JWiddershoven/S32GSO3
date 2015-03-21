c638cad08f598b430fb8a5d630a2298656a21bcf
Guzak, Jordy J.L.C <j.guzak@student.fontys.nl>
2015-03-21 16:59:12 +0100

    Unit tests voor TimeSpan class gemaakt

--- UnittestenTime/test/fontys/time/TestTimeSpan.java
+++ UnittestenTime/test/fontys/time/TestTimeSpan.java
@@ -61,6 +61,86 @@ public class TestTimeSpan {
      
    }
    
+   @Test public void testTimeSpan()
+   {
+      /* class invariant: 
+     * A stretch of time with a begin time and end time.
+     * The end time is always later then the begin time; the length of the time span is
+     * always positive
+     * 
+     */
+       try {
+           Time bt = new Time(2015, 1, 5, 12, 25);
+           Time et = new Time(2015, 1, 5, 13, 45);
+           TimeSpan ts = new TimeSpan(bt, et);
+       } catch (IllegalArgumentException e) {
+       }  
+       
+   }
+   
+   @Test(expected = IllegalArgumentException.class)
+   public void testTimeSpan2()
+   {
+     /* class invariant: 
+     * A stretch of time with a begin time and end time.
+     * The end time is always later then the begin time; the length of the time span is
+     * always positive
+     * 
+     */
+       Time bt = new Time(2015, 1, 5, 12, 25);
+       Time et = new Time(2015, 1, 5, 13, 45);
+       
+       TimeSpan ts = new TimeSpan(et, bt);
+   }
+   
+   @Test public void testGetBeginTime()
+   {
+     /**
+     * 
+     * @return the begin time of this time span
+     */
+       Time bt = new Time(2015, 1, 5, 12, 25);
+       Time et = new Time(2015, 1, 5, 13, 45);
+       
+       TimeSpan ts = new TimeSpan(bt, et);
+       
+       assertEquals(bt, ts.getBeginTime());
+   }
+   
+   @Test public void testGetEndTime()
+   {
+     /**
+     * 
+     * @return the end time of this time span
+     */
+       Time bt = new Time(2015, 1, 5, 12, 25);
+       Time et = new Time(2015, 1, 5, 13, 45);
+       
+       TimeSpan ts = new TimeSpan(bt, et);
+       
+       assertEquals(et, ts.getEndTime());
+   }
+   
+   @Test public void testLength()
+   {
+     /**
+     * beginTime will be the new begin time of this time span
+     * @param beginTime must be earlier than the current end time
+     * of this time span
+     */
+       Time bt = new Time(2015, 1, 5, 12, 25);
+       Time et = new Time(2015, 1, 5, 13, 45);
+       
+       TimeSpan ts = new TimeSpan(bt, et);
+       
+       int expected = 80;
+       int actual = ts.length();
+       
+       assertEquals(expected, actual);
+   }
+   
+   
+   
    @Test public void testUnionWith()
    {
        
