e69c87828209e79e3d9f2569ed1d0f36904a04a1
Jelle Widdershoven <jellewiddershoven@gmail.com>
2015-03-28 14:53:14 +0100

    Code toegevoegd van Frank

--- AEXBannerApplicatie/src/aexbannerapplicatie/AEXBanner.java
+++ AEXBannerApplicatie/src/aexbannerapplicatie/AEXBanner.java
@@ -5,57 +5,77 @@
  */
 package aexbannerapplicatie;
 
+import javafx.animation.AnimationTimer;
+import javafx.application.Application;
 import javafx.scene.paint.Color;
 import javafx.scene.Scene;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.stage.Stage;
 import javafx.scene.canvas.Canvas;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 
 /**
  *
  * @author Jelle
  */
-public class AEXBanner extends javafx.application.Application {
+public class AEXBanner extends Application {
 
-    private String koersen;
-    private GraphicsContext gc;
+    public static final int WIDTH = 1000;
+    public static final int HEIGHT = 100;
+    public static final int NANO_TICKS = 20000000; 
+    // FRAME_RATE = 1000000000/NANO_TICKS = 50;
 
-    /**
-     * Wijzig de koersen.
-     *
-     * @param koersen De huidige koersen.
-     */
-    public void setKoersen(String koersen) {
-        this.koersen = koersen;
-    }
+    private Text text;
+    private double textLength;
+    private double textPosition;
 
     @Override
-    public void start(Stage primaryStage) throws Exception {
-        Canvas canvas = new Canvas(1000, 100);
-        Font font = new Font("Arial", 20);
-       
-        StackPane root = new StackPane();
-        root.getChildren().add(canvas);
-        Scene scene = new Scene(root, 1000, 100);
+    public void start(Stage primaryStage) {
+        Font font = new Font("Arial", HEIGHT);
+        text = new Text();
+        text.setFont(font);
+        text.setFill(Color.BLACK);
+
+        Pane root = new Pane();
+        root.getChildren().add(text);
+        Scene scene = new Scene(root, WIDTH, HEIGHT);
+
+        primaryStage.setTitle("AEX banner");
         primaryStage.setScene(scene);
         primaryStage.show();
         primaryStage.toFront();
-        gc = canvas.getGraphicsContext2D();
-        gc.setFont(font);
-        gc.setFill(Color.BLACK);
-        gc.fillRect(0, 0, 1000, 100);
-        gc.setFill(Color.YELLOW);
-        gc.fillText("hallo", 500,20);
-        
+
+        // Start animation: text moves from right to left
+        AnimationTimer timer = new AnimationTimer() {
+            private long prevUpdate;
+
+            @Override
+            public void handle(long now) {
+                long lag = now - prevUpdate;
+                if (lag >= NANO_TICKS) {
+                    // calculate next location of text
+                    // todo
+                }
+            }
+
+            @Override
+            public void start() {
+                prevUpdate = System.nanoTime();
+                textPosition = WIDTH;
+                text.relocate(textPosition, 0);
+                setKoersen("Nothing to display");
+                super.start();
+            }
+        };
+        timer.start();
     }
-    
-     /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        launch(args);
+
+    public void setKoersen(String koersen) {
+        text.setText(koersen);
+        textLength = text.getLayoutBounds().getWidth();
     }
 
 }
--- AEXBannerApplicatie/src/aexbannerapplicatie/BannerController.java
+++ AEXBannerApplicatie/src/aexbannerapplicatie/BannerController.java
@@ -5,22 +5,46 @@
  */
 package aexbannerapplicatie;
 
+import java.util.Timer;
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 
 /**
  *
  * @author Jelle
  */
 public class BannerController extends Application {
-    
+
     private AEXBanner banner;
-    private IEffectenbeurs effectenbeurs;
-    
+//  private IEffectenBeurs effectenbeurs;
+
     @Override
-    public void start(Stage primaryStage) throws Exception {
+    public void start(Stage primaryStage) {
         banner = new AEXBanner();
-        banner.start(new Stage());
-        effectenbeurs = new MockEffectenBeurs();
+        //primaryStage acts as the common stage of the AEXBanner and the 
+        //BannerController:
+        banner.start(primaryStage);
+
+        //create a timer which polls every 2 seconds
+        Timer pollingTimer = null;
+        // todo
+
+        
+        //remove pollingTimer as soon as primaryStage is closing:
+        primaryStage.setOnCloseRequest((WindowEvent we) -> {
+            pollingTimer.cancel();
+        });
     }
+
+    /**
+     * unfortunately, is needed pro forma to act as runnable class
+     *
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+
 }
